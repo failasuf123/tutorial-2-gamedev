7312b3574b45cc5eb41b7ebfedb34837cf5facf8
KenKomKom <kenichikomala@gmail.com>
2025-02-08 19:50:22 +0700

    version updated to 4.3

--- scenes/PlatformBlue.gd
+++ scenes/PlatformBlue.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var speed = 500.0
+@export var speed = 500.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_up"):
-		move_local_y(-speed * delta)
+		move_local_y(-speed * delta, false)
 	
 	if Input.is_action_pressed("move_down"):
-		move_local_y(speed * delta)
+		move_local_y(speed * delta, false)
